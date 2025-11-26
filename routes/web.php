67d3de28fa3ab43557237f974bb67d31c233f965
DNovF <mtkheliog96.20@gmail.com>
2025-11-26 23:58:10 +0700

    asd

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('/transaksi', Transaksi::class)->middleware(['auth'])->name('transaks
 Route::get('/laporan', Laporan::class)->middleware(['auth'])->name('laporan');
 Route::get('/home', User::class)->middleware(['auth'])->name('user');
 Route::get('/cetak', [App\Http\Controllers\HomeController::class, 'cetak']);
+route::get('');
